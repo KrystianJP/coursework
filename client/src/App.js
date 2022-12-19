c57720bb318491d034de58f689c6c6c9baa14c55
krystianjp <krystianjp97@gmail.com>
2022-12-19 17:43:04 +0000

    logout system

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,32 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Test from "./components/Test";
 import "./styles.css";
 import axios from "axios";
+import React, { Component } from "react";
 
-function App() {
-  const [username, setUsername] = useState("");
-  const [token, setToken] = useState("");
-  const [busy, setBusy] = useState(true);
-  const [dropdown, setDropdown] = useState(false);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { username: "", token: "", busy: true, dropdown: false };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.loginOrLogout = this.loginOrLogout.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
 
-  useEffect(() => {
+  componentDidMount() {
     // getting token from local storage if it exists
     const tokenFromStorage = localStorage.getItem("token");
     if (tokenFromStorage) {
       // if token exists, set token to the one in local storage
-      setToken(tokenFromStorage);
+      this.setState({ token: tokenFromStorage });
     } else {
       // reset token in case old one is saved
-      setToken("");
+      this.setState({ token: "" });
     }
 
     if (tokenFromStorage) {
@@ -38,23 +41,21 @@ function App() {
       axios
         .get("/api/user", config)
         .then((response) => {
-          console.log(response);
-          setUsername(response.data.username);
-          setBusy(false);
+          this.setState({ username: response.data.username, busy: false });
         })
         .catch((error) => {
           console.error(error);
         });
     } else {
-      setBusy(false);
+      this.setState({ busy: false });
     }
-  }, []);
+  }
 
-  function logOut() {
+  logOut() {
     axios
       .delete("/api/logout")
-      .then((response) => {
-        setToken("");
+      .then(() => {
+        this.setState({ token: "" });
         localStorage.setItem("token", "");
         window.location.href = "/login";
       })
@@ -63,7 +64,7 @@ function App() {
       });
   }
 
-  function loginOrLogout(token) {
+  loginOrLogout(token) {
     if (!token) {
       return (
         <a className="login" href="/login">
@@ -73,12 +74,12 @@ function App() {
     } else {
       return (
         <div>
-          <button className="login" onClick={toggleDropdown}>
-            {username}
+          <button className="login" onClick={this.toggleDropdown}>
+            {this.state.username}
           </button>
-          {dropdown ? (
+          {this.state.dropdown ? (
             <div className="logout-dropdown">
-              <button className="dropdown-logout" onClick={logOut}>
+              <button className="dropdown-logout" onClick={this.logOut}>
                 Log out
               </button>
             </div>
@@ -88,40 +89,46 @@ function App() {
     }
   }
 
-  function toggleDropdown(e) {
+  toggleDropdown(e) {
     e.stopPropagation();
-    if (!dropdown) {
-      setDropdown(true);
+    if (!this.state.dropdown) {
+      this.setState({ dropdown: true });
     } else {
-      setDropdown(false);
+      this.setState({ dropdown: false });
     }
   }
 
-  if (!busy) {
-    return (
-      <Router>
-        <header onClick={() => setDropdown(false)}>
-          <a className="logo" href="/">
-            <span className="material-symbols-outlined">home</span>
-            To Do List
-          </a>
-          {loginOrLogout(token)}
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/test" element={<Test />} />
+  render() {
+    if (!this.state.busy) {
+      return (
+        <Router>
+          <header onClick={() => this.setState({ dropdown: false })}>
+            <a className="logo" href="/">
+              <span className="material-symbols-outlined">home</span>
+              To Do List
+            </a>
+            {this.loginOrLogout(this.state.token)}
+          </header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test />} />
 
-          <Route
-            path="/login"
-            element={!token ? <Login /> : <Navigate replace to="/" />}
-          />
-          <Route
-            path="/signup"
-            element={!token ? <Signup /> : <Navigate replace to="/" />}
-          />
-        </Routes>
-      </Router>
-    );
+            <Route
+              path="/login"
+              element={
+                !this.state.token ? <Login /> : <Navigate replace to="/" />
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                !this.state.token ? <Signup /> : <Navigate replace to="/" />
+              }
+            />
+          </Routes>
+        </Router>
+      );
+    }
   }
 }
 
