798638cce8384a458d466a9a2b2317186dded2cd
krystianjp <krystianjp97@gmail.com>
2023-02-08 23:05:27 +0000

    changed url

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -15,7 +15,7 @@ class Login extends Component {
     let body = { username: this.state.username, password: this.state.password };
 
     axios
-      .post("http://localhost:5000/api/login", body)
+      .post("coursework-production.up.railway.app/api/login", body)
       .then((response) => {
         if (!response.data.accessToken) {
           this.setState({ password: "", errors: [response.data.message] });
