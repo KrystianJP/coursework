6023862748fdc705b86a334ffb589d10f35f82bc
krystianjp <krystianjp97@gmail.com>
2023-02-08 22:35:26 +0000

    changed again

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.use(cors());
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
 
   // Request methods you wish to allow
   res.setHeader(
